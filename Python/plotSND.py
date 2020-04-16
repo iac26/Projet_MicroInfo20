612ab5e37eb24f87b32522ed3eabced8b713c635
iac26 <iacopo.sprenger@epfl.ch>
2020-04-16 16:42:24 +0200

    Update plotSND.py

--- Python/plotSND.py
+++ Python/plotSND.py
@@ -4,11 +4,14 @@ import sys
 import time
 import struct
 from threading import Thread
+import matplotlib.pyplot as plt
+
+data = []
 
 
 class serial_thread(Thread):
 
-    #init function called when the thread begins
+    # init function called when the thread begins
     def __init__(self, port):
         Thread.__init__(self)
         self.contReceive = True
@@ -25,101 +28,152 @@ class serial_thread(Thread):
             print('Cannot connect to the e-puck2')
             sys.exit(0)
 
-    #function called after the init
+    # function called after the init
     def run(self):
-        while(self.alive):
-            if(self.contReceive):
-                readFloatSerial(self.port)
+        while (self.alive):
+            if (self.contReceive):
+                global data
+                data = []
+                data.append(readFloatSerial(self.port))
+                data.append(readFloatSerial(self.port))
+                data.append(readFloatSerial(self.port))
+                if len(data[0]) == 1024 and len(data[1]) == 1024 and len(data[2]) == 1024:
+                    mic_1_plot.set_ydata(data[0])
+                    mic_2_plot.set_ydata(data[1])
+                    mic_3_plot.set_ydata(data[2])
+
+
+                self.tell_to_update_plot()
+                print(data)
             else:
-                #flush the serial
+                # flush the serial
                 self.port.read(self.port.inWaiting())
                 time.sleep(0.1)
 
-    #clean exit of the thread if we need to stop it
+    # clean exit of the thread if we need to stop it
     def stop(self):
         self.alive = False
         self.join()
-        if(self.port.isOpen()):
-            while(self.port.inWaiting() > 0):
+        if (self.port.isOpen()):
+            while (self.port.inWaiting() > 0):
                 self.port.read(self.port.inWaiting())
                 time.sleep(0.01)
             self.port.close()
 
+    def plot_updated(self):
+        self.need_to_update = False
 
-#reads the FFT in float32 from the serial
+    #tell if the plot need to be updated
+    def need_to_update_plot(self):
+        return self.need_to_update
+
+    def tell_to_update_plot(self):
+        self.need_to_update = True
+
+
+# reads the FFT in float32 from the serial
 def readFloatSerial(port):
-
     state = 0
 
-    while(state != 5):
+    while (state != 5):
 
-        #reads 1 byte
+        # reads 1 byte
         c1 = port.read(1)
-        #timeout condition
-        if(c1 == b''):
+        # timeout condition
+        if (c1 == b''):
             print('Timout...')
             return [];
 
-        if(state == 0):
-            if(c1 == b'S'):
+        if (state == 0):
+            if (c1 == b'S'):
                 state = 1
             else:
                 state = 0
-        elif(state == 1):
-            if(c1 == b'T'):
+        elif (state == 1):
+            if (c1 == b'T'):
                 state = 2
-            elif(c1 == b'S'):
+            elif (c1 == b'S'):
                 state = 1
             else:
                 state = 0
-        elif(state == 2):
-            if(c1 == b'A'):
+        elif (state == 2):
+            if (c1 == b'A'):
                 state = 3
-            elif(c1 == b'S'):
+            elif (c1 == b'S'):
                 state = 1
             else:
                 state = 0
-        elif(state == 3):
-            if(c1 == b'R'):
+        elif (state == 3):
+            if (c1 == b'R'):
                 state = 4
             elif (c1 == b'S'):
                 state = 1
             else:
                 state = 0
-        elif(state == 4):
-            if(c1 == b'T'):
+        elif (state == 4):
+            if (c1 == b'T'):
                 state = 5
             elif (c1 == b'S'):
                 state = 1
             else:
                 state = 0
 
-    #reads the size
-    #converts as short int in little endian the two bytes read
-    size = struct.unpack('<h',port.read(2)) 
-    #removes the second element which is void
-    size = size[0]  
+    # reads the size
+    # converts as short int in little endian the two bytes read
+    size = struct.unpack('<h', port.read(2))
+    # removes the second element which is void
+    size = size[0]
     print("size: {}".format(size))
 
-    #reads the data
-    rcv_buffer = port.read(size*4)
+    # reads the data
+    rcv_buffer = port.read(size * 4)
     data = []
 
-    #if we receive the good amount of data, we convert them in float32
-    if(len(rcv_buffer) == 4*size):
+    # if we receive the good amount of data, we convert them in float32
+    if (len(rcv_buffer) == 4 * size):
         i = 0
-        while(i < size):
-            data.append(struct.unpack_from('<f',rcv_buffer, i*4)[0])
-            i = i+1
+        while (i < size):
+            data.append(struct.unpack_from('<f', rcv_buffer, i * 4)[0])
+            i = i + 1
 
-        print(data)
+        #print(data)
         print('received !')
         return data
     else:
         print('Timout...')
         return []
 
+def update_plot_mic():
+    pass
 
-s = serial_thread("com7")
-s.start()
+def update_plot():
+    if (reader.need_to_update_plot()):
+        fig.canvas.draw_idle()
+        reader.plot_updated()
+
+def handle_close(evt):
+    reader.stop()
+
+
+
+fig, ax = plt.subplots(num=None, figsize=(10, 8), dpi=80)
+fig.canvas.set_window_title('Noisy plot')
+plt.subplots_adjust(left=0.1, bottom=0.25)
+fig.canvas.mpl_connect('close_event', handle_close)
+
+mic_graph = plt.subplot(211)
+mic_1_plot, = plt.plot(np.linspace(0, 6, 1024), lw=1, color='red')
+
+mic_2_plot, = plt.plot(np.linspace(3, 6, 1024), lw=1, color='green')
+
+mic_3_plot, = plt.plot(np.linspace(0, 3, 1024), lw=1, color='blue')
+
+reader = serial_thread("com7")
+reader.start()
+
+timer = fig.canvas.new_timer(interval=50)
+timer.add_callback(update_plot)
+timer.start()
+
+plt.show()
 
